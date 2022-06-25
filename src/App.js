bd346128115c33ac373276b70c672f36bc7e63be
Minhajul Islam <36797565+minhajul131@users.noreply.github.com>
2022-06-25 15:49:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var names = ['Adul', 'jolla', 'Uddin',]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}  
       </header>
+      <MyName name={names[2]}></MyName>
+      <MyName name="Monoar"></MyName>
+      <MyName name={names[1]}></MyName>
     </div>
   );
+}
+
+function MyName (props){
+  const nameStyle = {
+    border : '2px solid blue',
+    margin : '20px',
+    // padding : '50px',
+    // backgroundColor : 'black'
+  }
+  return (
+    <div style={nameStyle}>
+      <h1>The man {props.name}</h1>
+      <h3>hero</h3>
+    </div>
+  )
 }
 
 export default App;
